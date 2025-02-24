e59670e5e4eec2e6d3ae9b2ccde89425413b9a56
EvgenKh <hamitcevich@gmail.com>
2025-02-24 19:11:24 +0300

    refactor: migration loader file

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,11 +1,14 @@
-class Loader {
-    constructor(baseLink, options) {
+class Loader<T = Record<string, unknown>> {
+    private baseLink: string;
+    private options: T;
+
+    constructor(baseLink: string, options: T) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
-    getResp(
-        { endpoint, options = {} },
+    protected getResp(
+        { endpoint, options = {} }: { endpoint: string; options?: Partial<T> },
         callback = () => {
             console.error('No callback for GET response');
         }
@@ -13,7 +16,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    private errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,18 +26,18 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    private makeUrl(options: Partial<T>, endpoint: string): string {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
+            url += `${key}=${(urlOptions as Record<string, string | number>)[key]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    private load(method: string, endpoint: string, callback: (data: unknown) => void, options: Partial<T> = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
