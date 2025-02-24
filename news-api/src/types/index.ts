598cb4807978f7bae244692eeffa88bfbc52f1b3
EvgenKh <hamitcevich@gmail.com>
2025-02-24 17:02:22 +0300

    refactor: migration appView file & add interfaces

--- news-api/src/types/index.ts
+++ news-api/src/types/index.ts
@@ -12,6 +12,27 @@ export interface INews {
     publishedAt: string;
 }
 
+export type TResponse = {
+    cataegory: string;
+    country: string;
+    description: string;
+    id: string;
+    language: string;
+    name: string;
+    url: string;
+};
+
+interface IResponse {
+    status: string;
+}
+export interface IResponseNews extends IResponse {
+    results: number;
+    articles: Array<INews>;
+}
+
+export interface IResponceSources extends IResponse {
+    sources: Array<TResponse>;
+}
 export abstract class Drawer<T> {
     abstract draw(data: Array<T>): void;
 }
